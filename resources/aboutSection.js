fcab09ae7a416cf341a0f963271d9d0776c65feb
sree4hary <sree4hary@gmail.com>
2023-01-28 14:31:59 +0530

    combined homeSection into the navBar section

--- resources/aboutSection.js
+++ resources/aboutSection.js
@@ -1,17 +1,11 @@
 var i = 0;
 var txt1 = "Hi, there. I'm Sreehari Surabind. I'm an Aspiring Java Backend Developer.";
-var txt2 = "lhsfsf sjhfkjshf shfksjnhf khkahsflafhlafh khskfh skfhkshkahkhfklhav khsfbkjsfb kshfkbskfh"
 var speed = 50;
 
 function typeWriter() {
     if (i < txt1.length) {
-      document.getElementById("typeWriter").innerHTML += txt.charAt(i);
+      document.getElementById("typeWriter").innerHTML += txt1.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
-    } i = 0;
-    if (i < txt.length) {
-        document.getElementById("typeWriter").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-      }
+    }
 }
